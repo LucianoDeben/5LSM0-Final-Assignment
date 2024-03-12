f02aa0924dd8dd7ad63fedb3449a055cc64d8fd5
LucidCodeAI <lutchocode@gmail.com>
2024-03-12 16:11:47 +0100

    Add setup

--- run_main.sh
+++ run_main.sh
@@ -4,9 +4,9 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu
-#SBATCH --time=1:00:00
+#SBATCH --time=120:00:00
 
-cd /gpfs/home4/scur0755/FinalAssignment
+cd /gpfs/home4/scur0755/5LSM0-Final-Assignment/
 mkdir wandb/$SLURM_JOBID
 
 srun apptainer exec --nv /gpfs/work5/0/jhstue005/JHS_data/5lsm0_v1.sif /bin/bash run_container.sh
