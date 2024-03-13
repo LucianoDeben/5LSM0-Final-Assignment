744305533a6826410bc6ccd60ac5858baa34d247
LucidCodeAI <lutchocode@gmail.com>
2024-03-13 09:31:50 +0100

    Add keras install in bash scripy

--- run_container.sh
+++ run_container.sh
@@ -5,5 +5,8 @@ export WANDB_CACHE_DIR=wandb/$SLURM_JOBID
 export WANDB_START_METHOD="thread"
 wandb login
 
+# Install Keras
+pip install keras
+
 torchrun --nnodes=1 --nproc_per_node=1 train.py \
          --data_path "/gpfs/work5/0/jhstue005/JHS_data/CityScapes"
