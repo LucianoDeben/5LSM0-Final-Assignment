e2374e4c8059d237d367126beeb0c9c062c748ba
LucidCodeAI <lutchocode@gmail.com>
2024-03-14 10:57:18 +0100

    Remove mean IOU metric

--- train.py
+++ train.py
@@ -8,7 +8,6 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 from argparse import ArgumentParser
 
-import keras
 import matplotlib.pyplot as plt
 import torch
 from torch import nn
@@ -63,12 +62,11 @@ def get_data_loader(args, batch_size, num_workers):
 
     return train_loader, val_loader
 
-def train(train_dataloader, model, criterion, optimizer, device, m_iou):
+def train(train_dataloader, model, criterion, optimizer, device):
     model.train()
     train_loss = 0
     train_acc = 0
     train_dice = 0
-    train_m_iou = 0
     for inputs, targets in tqdm(train_dataloader):
         targets = utils.map_id_to_train_id(targets)
         inputs, targets = inputs.to(device), targets.to(device)
@@ -82,16 +80,14 @@ def train(train_dataloader, model, criterion, optimizer, device, m_iou):
         train_loss += loss.item()
         train_acc += pixel_accuracy(outputs, targets).item()
         train_dice += dice_coefficient(outputs, targets).item()
-        train_m_iou += float(mean_iou(mean_iou=m_iou, output=outputs, target=targets))
 
-    return train_loss / len(train_dataloader), train_acc / len(train_dataloader), train_dice / len(train_dataloader), train_m_iou / len(train_dataloader)
+    return train_loss / len(train_dataloader), train_acc / len(train_dataloader), train_dice / len(train_dataloader)
 
-def validate(val_dataloader, model, criterion, device, m_iou):
+def validate(val_dataloader, model, criterion, device):
     model.eval()
     val_loss = 0
     val_acc = 0
     val_dice = 0
-    val_m_iou = 0
     with torch.no_grad():
         for inputs, targets in tqdm(val_dataloader):
             inputs, targets = inputs.to(device), targets.to(device)
@@ -102,8 +98,7 @@ def validate(val_dataloader, model, criterion, device, m_iou):
             val_loss += loss.item()
             val_acc += pixel_accuracy(outputs, targets).item()
             val_dice += dice_coefficient(outputs, targets).item()
-            val_m_iou += float(mean_iou(mean_iou=m_iou, output=outputs, target=targets))
-    return val_loss / len(val_dataloader), val_acc / len(val_dataloader), val_dice / len(val_dataloader), m_iou.result()
+    return val_loss / len(val_dataloader), val_acc / len(val_dataloader), val_dice / len(val_dataloader)
 
 
 def main(args):
@@ -130,10 +125,6 @@ def main(args):
     # Define the number of epochs
     n_epochs = config.num_epochs
     
-    # Define the mean IOU metric
-    m_iou_train = keras.metrics.MeanIoU(num_classes=34)
-    m_iou_val = keras.metrics.MeanIoU(num_classes=34)
-    
     # Training loop
     train_losses = []
     val_losses = []
@@ -141,35 +132,29 @@ def main(args):
     val_accs = []
     train_dices = []
     val_dices = []
-    train_mious = []
-    val_mious = [] 
     
     wandb.watch(model, log_freq=100)
 
     for epoch in range(n_epochs):
         print(f"Epoch {epoch+1}\n-------------------------------")
-        train_loss, train_acc, train_dice, train_miou = train(train_loader, model, criterion, optimizer, device, m_iou_train)
-        val_loss, val_acc, val_dice, val_miou = validate(val_loader, model, criterion, device, m_iou_val)
+        train_loss, train_acc, train_dice = train(train_loader, model, criterion, optimizer, device)
+        val_loss, val_acc, val_dice = validate(val_loader, model, criterion, device)
         train_losses.append(train_loss)
         val_losses.append(val_loss)
         train_accs.append(train_acc)
         val_accs.append(val_acc)
         train_dices.append(train_dice)
         val_dices.append(val_dice)
-        train_mious.append(train_miou)
-        val_mious.append(val_miou)
         
         wandb.log({
             "epoch": epoch,
     "train_loss": train_loss,
-    "train_mean_iou": train_miou,
     "train_acc": train_acc,
     "train_dice": train_dice
     })
         wandb.log({
             "epoch": epoch,
     "val_loss": val_loss,
-    "val_mean_iou": val_miou,
     "val_acc": val_acc,
     "val_dice": val_dice
     })
