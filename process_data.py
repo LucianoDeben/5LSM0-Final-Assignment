5476d34146ef5cb3ab5b7d72b97c04e17a898675
LucidCodeAI <lutchocode@gmail.com>
2024-04-04 09:34:58 +0200

    Add process update

--- process_data.py
+++ process_data.py
@@ -24,24 +24,37 @@ def preprocess(img):
             Resize((256, 256)),
             PILToTensor(),
             ConvertImageDtype(torch.float32),
-            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            # Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ]
     )
     img = transform(img)
-    # img = img.unsqueeze(0)
+    img = img.unsqueeze(0)
 
     return img
 
 
 def postprocess(prediction, shape=(256, 256)):
+    # Apply softmax to get probabilities
     prediction = torch.nn.functional.softmax(prediction, dim=1)
+
+    # Get the class with the highest probability for each pixel
     prediction = torch.argmax(prediction, dim=1)
+
+    # Convert the tensor to a floating point type
     prediction = prediction.float()
+
+    # Add an extra dimension for the number of channels
     prediction = prediction.unsqueeze(0)
-    prediction = F.resize(
-        prediction, size=shape, interpolation=InterpolationMode.NEAREST
+
+    # Resize prediction to original image size
+    prediction = F.interpolate(
+        prediction, size=shape, mode="bilinear", align_corners=False
     )
+
+    # Remove the extra dimension
     prediction = prediction.squeeze(0)
+
+    # Convert the tensor to a numpy array
     prediction = prediction.cpu().detach().numpy()
 
     return prediction
